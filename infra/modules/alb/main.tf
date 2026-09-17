38cacaedf745d70084a0a7a431a1694b0af7f53a
YacinD <Yacin.Djama@hotmail.com>
2026-09-17 11:39:04 +0100

    update: add HTTP to HTTPS redirect listener on ALB

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -73,3 +73,19 @@ resource "aws_lb_listener" "https" {
     target_group_arn = aws_lb_target_group.this.arn
   }
 }
+
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
